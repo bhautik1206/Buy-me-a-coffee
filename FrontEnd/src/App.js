3723b818f4f140c63e020a79553fc7440c145dcf
Bhautik <bhautikkapadiya06@gmail.com>
2024-05-13 22:58:02 +0530

    fix(prettier):applying prettier

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import React  from "react"
-import {  Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Navigation from './module/Navigation/Navigation';
 
 const App = () => {
-  const location=useLocation();
+  const location = useLocation();
 
   return (
     <div>
       <Routes location={location}>
-        <Route /> 
-      </Routes>     
+        <Route element={<Navigation />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
